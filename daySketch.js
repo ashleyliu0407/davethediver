3a5669086d8b7eda2bcff72f61dbfc9325ba8e59
tgupta30 <trishagupta368@gmail.com>
2025-11-01 17:41:04 -0400

    added day variable

--- daySketch.js
+++ daySketch.js
@@ -1,9 +1,15 @@
 let backgroundImg;
+let gameData = JSON.parse(localStorage.getItem("gameData")) || {
+  day: 1,
+  coins: 0,
+  inventory: []
+};
+
 
 //TEXT DETAILS
 //change when have local var!!
-let day = 1;  
-let message = "Day " + day;
+// let day = parseInt(localStorage.getItem("day")) || 1;
+let message = "Day " + gameData.day;
 //x pos 
 let x; 
 //y pos
@@ -13,6 +19,7 @@ let slideSpeed = 10;
 //frames until starts sliding 
 let startDelay = 100; 
 let frameCounter = 0;
+let dayIncremented = false; 
 
 
 //button 
@@ -29,6 +36,7 @@ function preload(){
 function setup(){
     createCanvas(1400,800);
     textAlign(CENTER, CENTER);
+    localStorage.removeItem("gameData");
     textSize(250);
     textFont("Quantico, sans-serif");
     fill(255);
@@ -45,19 +53,29 @@ function draw(){
   image(backgroundImg, 0, 0, width, height);
   frameCounter++;
   // wait a bit before sliding
-  if (frameCounter > startDelay) {
+  if (frameCounter > startDelay  && !dayIncremented) {
     x += slideSpeed;
+   
     
   }
 
   if(frameCounter > 200){
-     window.location.href = "boatStart.html";  // change this to your desired page
+    window.location.href = "boatStart.html";  // change this to your desired page
+    gameData.day++;
+    localStorage.setItem("gameData", JSON.stringify(gameData));
+    dayIncremented = true; 
   }
 
   // draw text
   text(message, x, y);
-  
-  
+
   
 }
+
+
+// function nextDay() {
+//   day++;
+//   localStorage.setItem("day", day);
+//   console.log("Day updated:", day);
+// }
 
